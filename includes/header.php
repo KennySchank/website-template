c545f08cb06f6460e06fff21b4fae83e1566ced4
KennySchank <ksschank@gmail.com>
2017-03-11 17:00:43 -0700

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title><?php echo $title ?></title>
+  <link rel="stylesheet" type="text/css" href="styles/main.min.css">
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php $title = "Webpage Title" ?>
+
+<?php include("includes/header.php"); ?>  /* Contains HTML head element and its children. Place navigation and HTML header element in this fileimag. */
+
+<main>
+</main>
+
+<?php include("includes/footer.php"); ?>  /* Contains HTML footer element */
